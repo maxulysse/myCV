8794c5954bdcfc6e32c378eca7ed9becab24c4e0
Maxime Garcia <max.u.garcia@gmail.com>
2017-10-13 17:28:14 +0200

    update singularity and nextflow version in tests

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
+set -xeuo pipefail
+
 TOOL="all"
 
-while [[ $# -gt 1 ]]
+while [[ $# -gt 0 ]]
 do
-  key="$1"
+  key=$1
   case $key in
     -t|--tool)
-    TOOL="$2"
-    shift
+    TOOL=$2
+    shift # past argument
+    shift # past value
     ;;
     *) # unknown option
+    shift # past argument
     ;;
   esac
-  shift
 done
 
 # Install Nextflow
-if [[ "$TOOL" = nextflow ]] || [[ "$TOOL" = all ]]
+if [[ all,nextflow =~ $TOOL ]]
 then
-  cd $HOME
+  cd ${HOME}
   curl -fsSL get.nextflow.io | bash
   chmod +x nextflow
   sudo mv nextflow /usr/local/bin/
 fi
 
 # Install Singularity
-if [[ "$TOOL" = singularity ]] || [[ "$TOOL" = all ]]
+if [[ all,singularity =~ $TOOL ]]
 then
-  cd $HOME
-  wget https://github.com/singularityware/singularity/releases/download/$SGT_VER/singularity-$SGT_VER.tar.gz
-  tar xvf singularity-$SGT_VER.tar.gz
-  cd singularity-$SGT_VER
+  cd ${HOME}
+  wget https://github.com/singularityware/singularity/releases/download/${SGT_VER}/singularity-${SGT_VER}.tar.gz
+  tar xvf singularity-${SGT_VER}.tar.gz
+  cd singularity-${SGT_VER}
   ./configure --prefix=/usr/local
   make
   sudo make install
   cd ..
-  rm -rf singularity-$SGT_VER*
+  rm -rf singularity-${SGT_VER}*
 fi
