fb691fe4bd22d4f63f214afc296b61a86a8af279
Maxime Garcia <max.u.garcia@gmail.com>
2018-07-18 14:46:32 +0200

    clean up code

--- scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-curl -fsSL get.nextflow.io | bash
-chmod +x nextflow
-sudo mv nextflow /usr/local/bin/
--- scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-for i in `ls -1 20*/*.tex` ; do
-  nextflow run MaxUlysse/compile-latex --tex $i --name CV-MGarcia-latest.pdf --outDir $PWD
-done
